2d0041060e5c60a07e78cfa0721b87433f658c07
Oliver Moser <omoser@ombook.fritz.box>
2016-10-21 11:38:02 +0200

    add test

--- s3/s3test.py
+++ s3/s3test.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 import boto3
+import json
 
 session = boto3.Session(profile_name="codecentric")
 s3 = session.client("s3")
 kms = session.client("kms")
 
-print(kms.list_keys())
+print(json.dumps(kms.list_keys(), indent=2))
